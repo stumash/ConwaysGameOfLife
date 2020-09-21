e3da44ac4556ad270994797530cbf6c489fbd834
Stuart Mashaal <smashaal@hopper.com>
2020-09-21 01:56:18 -0400

    publish to /docs folder

---
+++ docs/precache-manifest.eae51049a7d1b898b00e07f4c1e9c178.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6970a98a2759dd1c6aa8dd704b10e092",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e53a6df848575e38470c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4e5d36587d3ee910f740",
+    "url": "/static/js/2.0ef6c10c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ef6c10c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e53a6df848575e38470c",
+    "url": "/static/js/main.5b81f11e.chunk.js"
+  },
+  {
+    "revision": "85d367d40f4625aef104",
+    "url": "/static/js/runtime-main.1281186e.js"
+  }
+]);
