af0cc33b289e2baae7c9bf0bf261212883985604
David Huculak <davidhuculak5@gmail.com>
2020-09-21 22:35:02 -0400

    convert to functional component + hooks style; fix issue where setState isnt called with callback type arg; add small performance improvement to renderer

--- src/App.js
+++ src/App.js
@@ -1,103 +1,123 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 import { makeMatrix } from "./utils.js";
-import { updateGameOfLife, animateGameOfLife } from "./conwayUtils.js";
+import { updateGameOfLife, animateGameOfLife, drawGridLines } from "./conwayUtils.js";
 import constants from "./constants.js";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      CELL_SIZE: 10,
-      CANVAS_WIDTH: 50,
-      CANVAS_HEIGHT: 50,
-      GAME_RUNNING: false,
-      CLOCK_MILLIS: 500
-    };
+const CELL_SIZE = 10
+const CANVAS_WIDTH = 50
+const CANVAS_HEIGHT = 50
+const CLOCK_MILLIS = 500
 
-    this.clockID = undefined;
-    this.canvasRef = React.createRef();
-    this.grid = makeMatrix({
-      dims: [this.state.CANVAS_WIDTH, this.state.CANVAS_HEIGHT],
-      defaultValue: false
-    });
+const MERGED_CONSTANTS = {
+  CELL_SIZE, CANVAS_WIDTH, CANVAS_HEIGHT, CLOCK_MILLIS, ...constants,
+}
 
-    // start a pentadecathlon by default
-    const alive = [
-      [25, 9],
-      [25, 10],
-      [24, 11],
-      [26, 11],
-      [25, 12],
-      [25, 13],
-      [25, 14],
-      [25, 15],
-      [24, 16],
-      [26, 16],
-      [25, 17],
-      [25, 18]
-    ];
-    for (let [i, j] of alive) {
-      this.grid[i][j] = true;
-    }
+const INITIAL_GRID = (() => {
+  const grid = makeMatrix({
+    dims: [CANVAS_WIDTH, CANVAS_HEIGHT],
+    defaultValue: false
+  })
+  // start a pentadecathlon by default
+  const alive = [
+    [25, 9],
+    [25, 10],
+    [24, 11],
+    [26, 11],
+    [25, 12],
+    [25, 13],
+    [25, 14],
+    [25, 15],
+    [24, 16],
+    [26, 16],
+    [25, 17],
+    [25, 18]
+  ];
+  for (let [i, j] of alive) {
+    grid[i][j] = true;
   }
+  return grid
+})()
 
-  buttonText = () => {
-    return this.state.GAME_RUNNING ? "stop" : "run";
-  };
-  toggleGameRunning = () => {
-    const wasRunning = this.state.GAME_RUNNING;
-    this.setState({
-      ...this.state,
-      GAME_RUNNING: !this.state.GAME_RUNNING
-    });
-    if (wasRunning) {
-      clearInterval(this.clockID);
-    } else {
-      // start the game
-      const c = this.canvasRef.current.getContext("2d");
-      animateGameOfLife(this.grid, c, { ...this.state, ...constants });
-      this.clockID = setInterval(() => {
-        updateGameOfLife(this.grid);
-        animateGameOfLife(this.grid, c, { ...this.state, ...constants });
-      }, this.state.CLOCK_MILLIS);
+const App = () => {
+  const gridRef = useRef(INITIAL_GRID);
+  const gameCanvasRef = useRef(null);
+  const gridCanvasRef = useRef(null);
+
+  const [isGameRunning, setIsGameRunning] = useState(false);
+  const toggleIsGameRunning = () => setIsGameRunning((wasGameRunning) => !wasGameRunning);
+
+  useEffect(() => {
+    drawGridLines(gridCanvasRef.current.getContext("2d"), MERGED_CONSTANTS)
+  })
+
+  useEffect(() => {
+    // dont start the loop if the game isnt running
+    if (!isGameRunning) {
+      return;
     }
-  };
 
-  onCanvasClick = evt => {
+    // start the loop
+    const grid  = gridRef.current
+    const c = gameCanvasRef.current.getContext("2d");
+    animateGameOfLife(grid, c, MERGED_CONSTANTS);
+    const clockID = setInterval(() => {
+      updateGameOfLife(grid);
+      animateGameOfLife(grid, c, MERGED_CONSTANTS);
+    }, CLOCK_MILLIS);
+
+    // cancel the loop when isGameRunning's value changes
+    return () => clearInterval(clockID)
+  }, [isGameRunning]);
+
+  const handleCanvasClicked = (evt) => {
+    if (!isGameRunning) {
+      return
+    }
     evt.stopPropagation();
-    const rect = this.canvasRef.current.getBoundingClientRect();
+    const canvas = gameCanvasRef.current
+    const grid = gridRef.current
+    const rect = canvas.getBoundingClientRect();
     const x = evt.nativeEvent.clientX - rect.left;
     const y = evt.nativeEvent.clientY - rect.top;
 
-    const i = Math.floor(x / this.state.CELL_SIZE);
-    const j = Math.floor(y / this.state.CELL_SIZE);
+    const i = Math.floor(x / CELL_SIZE);
+    const j = Math.floor(y / CELL_SIZE);
 
-    if (i >= 0 && j >= 0 && i < this.grid.length && j < this.grid[0].length) {
-      this.grid[i][j] = !this.grid[i][j];
+    if (i >= 0 && j >= 0 && i < grid.length && j < grid[0].length) {
+      grid[i][j] = !grid[i][j];
     }
-    animateGameOfLife(this.grid, this.canvasRef.current.getContext("2d"), { ...this.state, ...constants });
-  };
+    animateGameOfLife(grid, canvas.getContext("2d"), MERGED_CONSTANTS);
+  }
 
-  render = () => (
+  return (
     <div className="App">
       <header className="App-header">
         <div>Conway's Game of Life</div>
-        <canvas
-          width={this.state.CANVAS_WIDTH * this.state.CELL_SIZE}
-          height={this.state.CANVAS_HEIGHT * this.state.CELL_SIZE}
-          ref={this.canvasRef}
-          onClick={this.onCanvasClick}
-        ></canvas>
-
+        <div className="canvasContainer" width={CANVAS_WIDTH * CELL_SIZE}
+            height={CANVAS_HEIGHT * CELL_SIZE}>
+          <canvas
+            width={CANVAS_WIDTH * CELL_SIZE}
+            height={CANVAS_HEIGHT * CELL_SIZE}
+            ref={gameCanvasRef}
+            onClick={handleCanvasClicked}
+            className="gameCanvas"
+          ></canvas>
+          <canvas
+            width={CANVAS_WIDTH * CELL_SIZE}
+            height={CANVAS_HEIGHT * CELL_SIZE}
+            ref={gridCanvasRef}
+            className="gridCanvas"
+          ></canvas>
+        </div>
         <div id="control-panel">
-          <button type="button" onClick={this.toggleGameRunning}>
-            {this.buttonText()}
+          <button type="button" onClick={toggleIsGameRunning}>
+            {isGameRunning ? "stop" : "run"}
           </button>
         </div>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
